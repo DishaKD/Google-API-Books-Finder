9c0aa8d323b41376d769c0464ea68a47a0e1889d
DishaKD <dasanayakedishal@gmail.com>
2025-01-24 01:53:59 +0530

    implement route

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { getBooks } = require("../controllers/bookcontroller");
+const { getBooks, getBookById } = require("../controllers/bookcontroller");
 const router = express.Router();
 
+//Retrieve all books
 router.get("/", getBooks);
+
+//Retrieve books by ID
+router.get("/:id", getBookById);
 
 module.exports = router;
