18704cabad62978f68264a98fe001eaf55c6803d
DishaKD <dasanayakedishal@gmail.com>
2025-01-24 01:53:06 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 const app = require("./app");
-const runMigrations = require("./database/migrations");
-const fetchAndSaveBooks = require("./services/googleBooksService");
+const fetchAndSaveBooks = require("./src/services/googleBooksService");
 
 const PORT = 5000;
 
 // Run migrations and fetch books on server start
-runMigrations().then(() => fetchAndSaveBooks());
+fetchAndSaveBooks();
 
 app.listen(PORT, () => {
   console.log(`Backend running at http://localhost:${PORT}`);
