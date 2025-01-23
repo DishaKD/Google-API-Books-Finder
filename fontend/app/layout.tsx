cd273cf58170ef4a19b733c738cef2344a825619
DishaKD <dasanayakedishal@gmail.com>
2025-01-24 04:24:47 +0530

    update nav path

--- fontend/app/layout.tsx
+++ fontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navigation from "./composnents/navbar";
+import Navigation from "./components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
